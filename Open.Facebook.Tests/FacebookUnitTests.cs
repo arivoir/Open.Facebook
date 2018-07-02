e12b57d921e2369181bfd22032e38a7584eeabf2
Alvaro Rivoir <arivoir@gmail.com>
2018-07-02 15:10:07 -0300

    refactoring and test case project added

---
+++ Open.Facebook.Tests/FacebookUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.IO;
+
+namespace Open.Facebook.Tests
+{
+    [TestClass]
+    public class FacebookUnitTests
+    {
+        [TestMethod]
+        public async Task UploadPhoto()
+        {
+            string accessToken = "";
+            var client = new FacebookClient(accessToken);
+            System.IO.Stream fileStream = null;
+            var progressList = new List<StreamProgress>();
+            var progress = new Progress<StreamProgress>(p =>
+            {
+                progressList.Add(p);
+            });
+            var photo = await client.UploadPhotoAsync("", "", fileStream, progress, CancellationToken.None);
+            Assert.IsTrue(progressList.Count > 0);
+        }
+    }
+}
---
+++ Open.Facebook.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Open.Facebook.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Open.Facebook.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49b1939e-7041-47fe-9b69-2be48562f326")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
